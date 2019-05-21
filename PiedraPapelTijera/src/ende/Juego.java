d8dfdd38af3c9dce6bb481f3babe25f266e97191
Isakas <inakibengo17@gmail.com>
2019-05-21 14:21:17 +0200

    Jugador cambiado de posicion y las variables las e puesto arriba

--- PiedraPapelTijera/src/ende/Juego.java
+++ PiedraPapelTijera/src/ende/Juego.java
@@ -1,6 +1,43 @@
 package ende;
 
 /**
+*
+*/
+class Jugador {
+
+	/**
+	 * Escoge piedra, papel o tijera al azar
+	 */
+
+	int exitos; // número de partidas ganadas
+	int winTotal;
+
+	public String opcion_al_azar() {
+		String opcion = "";
+		Integer c = (int) (Math.random() * 3);
+		switch (c) {
+		case 0:
+			opcion = ("piedra");
+			break;
+		case 1:
+			opcion = ("papel");
+			break;
+		case 2:
+			opcion = ("tijeras");
+		}
+		return opcion;
+	}
+
+	public void setÉxitos() {
+		exitos++;
+	}
+
+	public int getÉxitos() {
+		return (exitos);
+	}
+}
+
+/**
  * Simula el juego de Piedra, Papel o tijera
  */
 
@@ -56,40 +93,4 @@ public class Juego {
 			System.out.println();
 		}
 	}
-}
-
-/**
- *
- */
-class Jugador {
-
-	/**
-	 * Escoge piedra, papel o tijera al azar
-	 */
-	public String opcion_al_azar() {
-		String opcion = "";
-		Integer c = (int) (Math.random() * 3);
-		switch (c) {
-		case 0:
-			opcion = ("piedra");
-			break;
-		case 1:
-			opcion = ("papel");
-			break;
-		case 2:
-			opcion = ("tijeras");
-		}
-		return opcion;
-	}
-
-	public void setÉxitos() {
-		exitos++;
-	}
-
-	public int getÉxitos() {
-		return (exitos);
-	}
-
-	int exitos; // número de partidas ganadas
-	int winTotal;
 }
